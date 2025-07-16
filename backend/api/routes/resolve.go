b86d7954e1f0a38d8296748b958235cf9e9832a4
Rashi-Dwivedi1812 <rashidwivedi1812@gmail.com>
2025-07-16 16:18:40 +0530

    updating resolve func

--- backend/api/routes/resolve.go
+++ backend/api/routes/resolve.go
@@ -2,30 +2,32 @@ package routes
 
 import (
 	"Url-Shortener/database"
-
-	// "github.com/go-redis/redis/v8"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 )
 
-func ResolveURL(c *fiber.Ctx)error{
+func ResolveURL(c *fiber.Ctx) error {
 	url := c.Params("url")
-	r := database.CreateClient((0))
+	r := database.CreateClient(0)
 	defer r.Close()
 
-	value,err :=r.Get(database.Ctx,url).Result()
-	if err!=nil{
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		// Key does not exist
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"error":"short not found in the database",
+			"error": "short not found in the database",
 		})
-	}else if err!= nil{
+	} else if err != nil {
+		// Real connection error
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error":"cannot connect to db",
+			"error": "cannot connect to db",
 		})
 	}
+
+	// Increment usage counter
 	rInr := database.CreateClient(1)
 	defer rInr.Close()
+	_ = rInr.Incr(database.Ctx, "counter")
 
-	_= 	rInr.Incr(database.Ctx,"counter")
-
-	return c.Redirect(value,301)
+	return c.Redirect(value, 301)
 }
